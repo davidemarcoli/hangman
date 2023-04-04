0a134176296ed94fcff0d37a63ebfb5c252d7ec2
davidemarcoli <davide@marcoli.ch>
2023-04-04 11:15:44 +0200

    Add select for the wordlists Add german wordlist

--- hangman.py
+++ hangman.py
@@ -1,16 +1,18 @@
 import os
 import random
-from string import ascii_uppercase
 from tkinter import *
 from tkinter import messagebox
+from string import ascii_uppercase
 
 window = Tk()
 window.title('Hangman Game')
 
+MAX_NUMER_OF_GUESSES = 11
+
 
 def loadImages():
     loadedImages = []
-    for i in range(12):
+    for i in range(MAX_NUMER_OF_GUESSES + 1):
         loadedImages.append(PhotoImage(file="images/hang" + str(i) + ".png"))
     return loadedImages
 
@@ -18,14 +20,27 @@ def loadImages():
 images = loadImages()
 
 
+# def loadRandomWord():
+#     # load all files from the folder "wordlists"
+#     files = os.listdir("wordlists")
+#     # choose a random file
+#     file = random.choice(files)
+#     print("The word is in the file: " + file)
+#     # load the file
+#     with open("wordlists/" + file, "r") as f:
+#         words = f.read().splitlines()
+#     # choose a random word from the file
+#     word = random.choice(words)
+#     print("The word is: " + word)
+#
+#     return word
+
 def loadRandomWord():
-    # load all files from the folder "wordlists"
-    files = os.listdir("wordlists")
-    # choose a random file
-    file = random.choice(files)
-    print("The word is in the file: " + file)
+    if selectedWordlist is None:
+        messagebox.showerror("Hangman", "Please select a wordlist")
+        return
     # load the file
-    with open("wordlists/" + file, "r") as f:
+    with open("wordlists/" + selectedWordlist, "r") as f:
         words = f.read().splitlines()
     # choose a random word from the file
     word = random.choice(words)
@@ -34,7 +49,7 @@ def loadRandomWord():
     return word
 
 
-def newGame():
+def resetGame():
     global the_word_withSpaces
     global numberOfGuesses
     numberOfGuesses = 0
@@ -51,7 +66,7 @@ def newGame():
 
 def guess(letter):
     global numberOfGuesses
-    if numberOfGuesses < 11:
+    if numberOfGuesses < MAX_NUMER_OF_GUESSES:
         txt = list(the_word_withSpaces)
         guessed = list(lblWord.get())
         if the_word_withSpaces.count(letter) > 0:
@@ -64,23 +79,44 @@ def guess(letter):
         else:
             numberOfGuesses += 1
             imgLabel.config(image=images[numberOfGuesses])
-            if numberOfGuesses == 11:
+            if numberOfGuesses == MAX_NUMER_OF_GUESSES:
                 messagebox.showwarning("Hangman", "Game Over")
 
 
+selectedWordlist = None
+
+
+# def askForWordlist():
+#     global selectedWordlist
+#     selectedWordlist = askopenfilename(initialdir="wordlists", title="Select a wordlist",
+#                                        filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
+
+def setWordlist(wordlist=None):
+    print("Wordlist: " + str(wordlist))
+    global selectedWordlist
+    if wordlist is None:
+        selectedWordlist = wordlistSelect.get()
+    else:
+        selectedWordlist = wordlist
+
+
+wordlistSelect = StringVar()
+wordlistSelect.set("Select a wordlist")
+wordlistMenu = OptionMenu(window, wordlistSelect, *os.listdir("wordlists"), command=setWordlist)
+wordlistMenu.grid(row=0, column=0, columnspan=3, padx=10, pady=40)
+
 imgLabel = Label(window)
-imgLabel.grid(row=0, column=0, columnspan=3, padx=10, pady=40)
+imgLabel.grid(row=1, column=0, columnspan=3, padx=10, pady=40)
 
 lblWord = StringVar()
-Label(window, textvariable=lblWord, font='consolas 24 bold').grid(row=0, column=3, columnspan=6, padx=10)
+Label(window, textvariable=lblWord, font='consolas 24 bold').grid(row=1, column=3, columnspan=6, padx=10)
 
 n = 0
 for c in ascii_uppercase:
-    Button(window, text=c, command=lambda c=c: guess(c), font='Helvetica 18', width=4).grid(row=1 + n // 9,
+    Button(window, text=c, command=lambda c=c: guess(c), font='Helvetica 18', width=4).grid(row=2 + n // 9,
                                                                                             column=n % 9)
     n += 1
 
-Button(window, text="New\nGame", command=lambda: newGame(), font="Helvetica 10 bold").grid(row=3, column=8)
+Button(window, text="New\nGame", command=lambda: resetGame(), font="Helvetica 10 bold").grid(row=4, column=8)
 
-newGame()
 window.mainloop()
